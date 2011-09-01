955685b691a28348c103820c796de3c05e243096
Marcin Benke <marcin@benke.org>
2011-09-01 11:39:05 +0200

    Tiny3: reorder definitions, cleanup

--- Tiny3/Interpreter.hs
+++ Tiny3/Interpreter.hs
@@ -8,24 +8,13 @@ import Control.Monad.State
 import Control.Monad.Trans
 import Control.Monad.Identity
 
-type Name = String
-
-type Store = Map.Map Loc Val
-initStore :: Store
-initStore = Map.empty
-
-type Loc = Int
-
-type Env = [Map.Map Name Loc]  -- a stack of maps
-initEnv :: Env
-initEnv = [Map.empty]
 
 data IntState = IntState { 
   store :: Store, 
   freeLocs :: [Loc], 
   env :: Env }
 
-initState = IntState initStore [1..2^16] initEnv
+initState = IntState initStore initFreeLocs  initEnv
 
 type IM a = StateT IntState (ErrorT String IO) a
 runIM :: IM a -> IntState -> IO (Either String (a,IntState))
@@ -38,14 +27,46 @@ runProg p = do
     Left e -> putStrLn e
     Right (a,state) -> print $ Map.toAscList (store state)
 
+-- * Store    
+    
+type Loc = Int
+type Store = Map.Map Loc Val
+initStore :: Store
+initStore = Map.empty
+
+alloc :: IM Loc
+alloc = do
+  locs <- getFreeLocs
+  case locs of
+    [] -> throwError "alloc: no more free locs"
+    (l:ls) -> putFreeLocs ls >> return l
+
+free :: Loc -> IM ()
+free l = do 
+  ls <- getFreeLocs
+  putFreeLocs (l:ls)
+
+updateStore :: Loc -> Val -> IM ()
+updateStore v x = modify $ \state -> state { 
+  store = Map.insert v x (store state)}
+
 getStore :: IM Store  
 getStore = gets store
+
+initFreeLocs :: [Loc]
+initFreeLocs = [1..2^16]
 
 getFreeLocs :: IM [Loc]
 getFreeLocs = gets freeLocs
 
 putFreeLocs :: [Loc] -> IM ()
 putFreeLocs freeLocs = modify $ \r -> r { freeLocs }
+
+-- * Names and Environment
+type Name = String
+type Env = [Map.Map Name Loc]  -- a stack of maps
+initEnv :: Env
+initEnv = [Map.empty]
 
 getEnv :: IM Env
 getEnv = gets env
@@ -65,10 +86,6 @@ leaveScope = do
   -- mapM_ free (Map.keys current)
   putEnv olderEnv
 
-mapHead :: (a->a) -> [a] -> [a]
-mapHead f (x:xs) = f x:xs
-mapHead f [] = []
-
 whenNothing :: Maybe a -> Maybe a -> Maybe a
 whenNothing Nothing y = y
 whenNothing x y = x
@@ -78,20 +95,9 @@ lookupEnv n [] = Nothing
 lookupEnv n (e:es) = Map.lookup n e `whenNothing` lookupEnv n es
   
 updateEnv :: Name -> Loc -> Env -> Env
-updateEnv n l = mapHead (Map.insert n l)
-
-alloc :: IM Loc
-alloc = do
-  locs <- getFreeLocs
-  case locs of
-    [] -> throwError "alloc: no more free locs"
-    (l:ls) -> putFreeLocs ls >> return l
-
-free :: Loc -> IM ()
-free l = do 
-  ls <- getFreeLocs
-  putFreeLocs (l:ls)
-
+updateEnv n l = mapHead (Map.insert n l) where
+  mapHead f (x:xs) = f x:xs
+  mapHead f [] = []
 
 getNameLoc :: Name -> IM Loc
 getNameLoc n =  do
@@ -106,21 +112,17 @@ getVar v =  do
   let res  = Map.lookup loc store
   maybe (throwError $ "Unallocated var"++ v) return res
 
-updateStore :: Loc -> Val -> IM ()
-updateStore v x = modify $ \state -> state { 
-  store = Map.insert v x (store state)}
-
+-- | Evaluate expressions
 eval :: Exp -> IM Val
 eval (EInt i) = return i
 eval (EVar s) = getVar s
 eval (EAdd e1 e2) = liftM2 (+) (eval e1) (eval e2)
 
+-- | Execute statements
 exec :: Stmt -> IM ()
 exec (SVar n) = do
   l <- alloc
-  (current:olderEnv) <- getEnv
   modifyEnv (updateEnv n l)
-  
   return ()
 exec (SBlock ss)= enterScope >> mapM_ exec ss >> leaveScope
 
