1e7e3292a3075fa534c9e20ffbce4ef831619c67
Marcin Benke <marcin@benke.org>
2011-09-01 14:27:19 +0200

    Tiny3: hopefully more efficient environment representation

--- Test.hs
+++ Test.hs
@@ -2,10 +2,13 @@ module Main where
 import qualified Tiny1.Interpreter as Tiny1
 import qualified Tiny2.Interpreter as Tiny2
 import qualified Tiny3.Interpreter as Tiny3
+import qualified Tiny3.Interpreter2 as Tiny3b
 
 main :: IO()
 main = do
   Tiny1.test
   Tiny2.test
   Tiny3.test
+  Tiny3b.test
+  
   
---
+++ Tiny3/Interpreter2.hs
@@ -0,0 +1,172 @@
+{-# LANGUAGE MultiParamTypeClasses, NamedFieldPuns #-}
+-- Tiny3.Interpreter defined environment as a stack of maps
+-- Here we use a map of stacks
+
+module Tiny3.Interpreter2 where
+import Tiny3.Syntax
+
+import qualified Data.Map as Map
+import Control.Monad.Error
+import Control.Monad.State
+import Control.Monad.Trans
+import Control.Monad.Identity
+import Data.Maybe(catMaybes)
+
+data IntState = IntState { 
+  store :: Store, 
+  freeLocs :: [Loc], 
+  env :: Env,
+  scopes :: Scopes}
+
+initState = IntState initStore initFreeLocs initEnv initScopes
+
+type IM a = StateT IntState (ErrorT String IO) a
+runIM :: IM a -> IntState -> IO (Either String (a,IntState))
+runIM m st = runErrorT (runStateT m st)
+
+runProg :: Stmt -> IO ()
+runProg p = do
+  res <- runIM (exec p) initState
+  case res of
+    Left e -> putStrLn e
+    Right (a,state) -> print $ Map.toAscList (store state)
+
+-- * Store    
+    
+type Loc = Int
+type Store = Map.Map Loc Val
+initStore :: Store
+initStore = Map.empty
+
+alloc :: IM Loc
+alloc = do
+  locs <- getFreeLocs
+  case locs of
+    [] -> throwError "alloc: no more free locs"
+    (l:ls) -> putFreeLocs ls >> return l
+
+free :: Loc -> IM ()
+free l = do 
+  ls <- getFreeLocs
+  putFreeLocs (l:ls)
+
+updateStore :: Loc -> Val -> IM ()
+updateStore v x = modify $ \state -> state { 
+  store = Map.insert v x (store state)}
+
+getStore :: IM Store  
+getStore = gets store
+
+initFreeLocs :: [Loc]
+initFreeLocs = [1..2^16]
+
+getFreeLocs :: IM [Loc]
+getFreeLocs = gets freeLocs
+
+putFreeLocs :: [Loc] -> IM ()
+putFreeLocs freeLocs = modify $ \r -> r { freeLocs }
+
+-- * Names and Environment
+type Name = String
+type Env = Map.Map Name [Loc]  -- a map name -> loc stack
+initEnv :: Env
+initEnv = Map.empty
+
+type Scope = [Name]
+type Scopes = [Scope]
+
+emptyScope = []
+initScopes = [emptyScope]
+
+getEnv :: IM Env
+getEnv = gets env
+
+putEnv :: Env -> IM ()
+putEnv env = modify $ \r -> r { env }
+
+modifyEnv :: (Env -> Env) -> IM ()
+modifyEnv f = getEnv >>= putEnv . f
+
+getScopes ::  IM Scopes
+getScopes = gets scopes
+
+putScopes :: Scopes -> IM ()
+putScopes scopes = modify $ \r -> r { scopes }
+
+modifyScopes :: (Scopes -> Scopes) -> IM ()
+modifyScopes f = getScopes >>= putScopes . f
+
+popScope :: IM Scope
+popScope = do
+  (scope:scopes) <- getScopes
+  putScopes scopes
+  return scope
+
+enterScope :: IM ()
+enterScope = modifyScopes (emptyScope:)
+
+leaveScope :: IM ()
+leaveScope = do
+  env <- getEnv
+  scope <- popScope
+  -- TODO: free scope vars
+  let scopeLocs = catMaybes $ map (flip lookupEnv env) scope
+  mapM_ free scopeLocs
+  let env' = foldr (\n e -> Map.update pop n e)env scope
+  putEnv env' where
+    pop :: [Loc] -> Maybe [Loc]
+    pop [] = Nothing
+    pop (x:xs) = Just xs
+
+addLocal :: Name -> Scopes -> Scopes
+addLocal n (h:t) =(n:h):t 
+addLocal n [] = []
+
+lookupEnv :: Name -> Env -> Maybe Loc
+lookupEnv n e = do
+  stack <- Map.lookup n e 
+  case stack of
+    [] -> Nothing
+    (l:_) -> return l
+  
+updateEnv :: Name -> Loc -> Env -> Env
+updateEnv n l = Map.insertWith (++) n [l] 
+
+getNameLoc :: Name -> IM Loc
+getNameLoc n =  do
+  env <- getEnv
+  let res  = lookupEnv n env
+  maybe (throwError $ unwords["Undefined var",n,"env is",show env]) return res
+  
+getVar :: Name -> IM Val
+getVar v =  do
+  loc <- getNameLoc v
+  store <- getStore 
+  let res  = Map.lookup loc store
+  maybe (throwError $ "Unallocated var"++ v) return res
+
+-- | Evaluate expressions
+eval :: Exp -> IM Val
+eval (EInt i) = return i
+eval (EVar s) = getVar s
+eval (EAdd e1 e2) = liftM2 (+) (eval e1) (eval e2)
+
+-- | Execute statements
+exec :: Stmt -> IM ()
+exec (SVar n) = do
+  l <- alloc
+  modifyEnv (updateEnv n l)
+  modifyScopes (addLocal n)
+  return ()
+exec (SBlock ss)= enterScope >> mapM_ exec ss >> leaveScope
+
+exec (v := e) = do
+  x <- eval e
+  loc <- getNameLoc v
+  updateStore loc x
+exec (SExp e) = eval e >>= liftIO . print 
+exec (SPrint e) = eval e >>= liftIO . print 
+-- exec x = error $ "exec unimplemented for " ++ show x
+
+test :: IO()
+test = runProg prog1
