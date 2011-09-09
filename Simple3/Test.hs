85a7fd63956f52285900ef30f97bd84724afd0c3
Marcin Benke <marcin@benke.org>
2011-09-09 15:07:27 +0200

    Simple3: added parser

--- Simple3/Test.hs
+++ Simple3/Test.hs
@@ -1,6 +1,7 @@
 module Simple3.Test where
 import Simple3.Syntax
 import Simple3.Interpreter
+import qualified Simple3.ParsecParser as Parser
 
 derefVar :: Name -> Exp
 derefVar = EDeref . EVar
@@ -11,8 +12,15 @@ test = do
  -- testInterpreter1
   putStrLn "  .../prog2"
   runProg prog2
+  testParser "text2" text2
   putStrLn "  .../prog3"
   runProg prog3
+
+testParser name text = case Parser.runParser name text of
+  Left e -> putStr "Parse error: " >> print e
+  Right p ->  do
+    putStrLn $ "Parsed OK: " ++ show p
+    runProg p
 
 prog2 :: Defs
 prog2 = [
@@ -30,3 +38,7 @@ prog3 = [
 -- undefined var
 bad1 = [("_", EVar "x")]
 
+text2 = "l=new 1 x=*l \
+\ _ = let x = 2 in if x then x else 42\
+\ y = 2\
+\ _ = x+y"
