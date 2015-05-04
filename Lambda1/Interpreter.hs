8821582cd83933b7e9400bdfe0b610fe16e1ef60
Marcin Benke <marcin.benke@gmail.com>
2015-05-04 11:25:18 +0200

    Lambda1.Interpreter.eval: use (!)

--- Lambda1/Interpreter.hs
+++ Lambda1/Interpreter.hs
@@ -1,7 +1,7 @@
 module Lambda1.Interpreter where
 import Lambda1.Syntax
 import qualified Data.Map as Map
-import Data.Map(Map)
+import Data.Map(Map, (!))
 import Data.Maybe(fromMaybe)
 import Control.Monad.Reader
 
@@ -13,7 +13,7 @@ type Env = Map Name Value
 
 eval :: Exp -> Env -> Value
 eval (EInt i) env =  (Vint i)
-eval (EVar v) env = fromMaybe (error "unknown variable") (Map.lookup v env) 
+eval (EVar v) env = env ! v
 eval e@(ELam _ _) env = Vclos e env
 eval (EApp e1 e2) env = apply (eval e1 env) (eval e2 env)
 
